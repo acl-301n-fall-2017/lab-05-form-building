6593acadf23be76823e0e03ac31fb45296e41c7b
J3ffcon1 <Jrlone1990@gmail.com>
2017-10-29 15:10:41 -0700

    todo #5 show tab-content page, todo#6 hide articles todo#7 added event handler to update preview. todo#8 created a variable for article attributes

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -75,26 +75,38 @@ articleView.setTeasers = function() {
 
 articleView.initNewArticlePage = function() {
   // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-
-
+    $('.tab-content').show();
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
-
+  $('#article-export').hide();
   $('#article-json').on('focus', function(){
     this.select();
   });
 
-  // TODO: Add an event handler to update the preview and the export field if any inputs change.
 
+  // TODO: Add an event handler to update the preview and the export field if any inputs change.
+    $('#newArticle').on('change', function() {
+
+      //update .articles
+    })
 };
 
 articleView.create = function() {
   // TODO: Set up a var to hold the new article we are creating.
   // Clear out the #articles element, so we can put in the updated preview
 
+    var newArticle = {
+      newArticle.title = $('title').val();
+      newArticle.author = $('author').val();
+      newArticle.authorUrl = $('authorUrl').val();
+      newArticle.category = $('category').val();
+      newArticle.body = $('body').val();
+      newArticle.publishedOn =$('publishedOn').val();
+    };
 
   // TODO: Instantiate an article based on what's in the form fields:
 
+    JSON.stringify(newArticle);
 
   // TODO: Use our interface to the Handblebars template to put this new article into the DOM:
 
