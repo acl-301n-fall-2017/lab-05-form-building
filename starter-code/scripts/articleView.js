ea8cb8f3ce59af99e969f38b8bba054ce84d4f1c
Mario Quintana <pyrros2000@gmail.com>
2018-05-08 18:45:34 -0700

    still working on TODOS

--- starter-code/scripts/articleView.js
+++ starter-code/scripts/articleView.js
@@ -74,17 +74,17 @@ articleView.setTeasers = function() {
 };
 
 articleView.initNewArticlePage = function() {
-  // TODO: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
-  
+  // TODONE: Ensure the main .tab-content area is revealed. We might add more tabs later or otherwise edit the tab navigation.
+  articleView.handleMainNav();
   // TODO: The new articles we create will be copy/pasted into our source data file.
   // Set up this "export" functionality. We can hide it for now, and show it once we have data to export.
-
+  $('#export-field').hide();
   $('#article-json').on('focus', function(){
     this.select();
   });
 
-  // TODO: Add an event handler to update the preview and the export field if any inputs change.
-  
+  // TODONE: Add an event handler to update the preview and the export field if any inputs change.
+  hljs.initHighlightingOnLoad();
   $('#new-blog').on('change', function(){
     articleView.create();
   });
@@ -109,19 +109,22 @@ articleView.create = function() {
   $('#articles').append(article.toHtml());
 
   // TODO: Activate the highlighting of any code blocks; look at the documentation for hljs to see how to do this by placing a callback function in the .each:
-  
-    var article = $('#articles').val();
-    var markedArticle = marked(article);
-    $('section#articles').html(markedArticle);
-    $('section#articles pre code').each(function(i, block) {
+
+    $('pre code').each(function(i, block) {
       hljs.highlightBlock(block);
-  
-  // TODO: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
-  
-});
-}
+    });
+
+  // TODONE: Show our export field, and export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
+  $('#export-field').show();
+  $('#article-json').val(JSON.stringify(article));
+};
+
+
 
 articleView.initIndexPage = function() {
+Article.all.forEach(function(article) {
+$('#articles').append(article.toHtml())
+});
   articleView.populateFilters();
   articleView.handleCategoryFilter();
   articleView.handleAuthorFilter();
